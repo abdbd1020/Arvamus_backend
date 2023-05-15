33a465ecbd1533d2a7fa24af6e3a578a17909275
Abdullah Ibne Masud <2018-525-313@student.cse.du.ac.bd>
2023-05-15 21:56:31 +0600

    done

--- src/api/dashboard.js
+++ src/api/dashboard.js
@@ -4,8 +4,8 @@ const dashboardController = require("../controllers/dashboardController");
 
 const router = express.Router();
 
-router.get("/database_commit", dashboardController.databaseCommit);
-router.get("/drop_tables", dashboardController.dropTables);
+// router.get("/database_commit", dashboardController.databaseCommit);
+// router.get("/drop_tables", dashboardController.dropTables);
 router.get("/get_all_teachers", dashboardController.getAllTeachers);
 router.get("/get_all_staff", dashboardController.getAllStaff);
 router.get(
